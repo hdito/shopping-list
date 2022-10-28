5fee6e6f7410795b0b2f76444f5a58a7b0ceac28
hdito <hditow@gmail.com>
2022-10-29 00:54:30 +0300

    Fix bug with unpropogating events

--- src/pages/lists/ListsHeader.tsx
+++ src/pages/lists/ListsHeader.tsx
@@ -10,7 +10,7 @@ export const ListsHeader = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="pointer-events-none flex gap-2 items-center justify-between px-4 py-2 text-lg shadow-md shadow-slate-300 border-b-2 border-slate-300 z-10 sticky bg-white top-0  w-full overflow-hidden">
+    <header className="flex gap-2 items-center justify-between px-4 py-2 text-lg shadow-md shadow-slate-300 border-b-2 border-slate-300 z-10 sticky bg-white top-0  w-full overflow-hidden">
       {loadingFirestoreUser ? (
         'Loading...'
       ) : (
