9866e1369c0ec0859740b6ce38bafb6030867002
hdito <hditow@gmail.com>
2022-10-29 13:24:29 +0300

    Add info and navigation to list's header

--- src/pages/list/ListHeader.tsx
+++ src/pages/list/ListHeader.tsx
@@ -1,30 +1,31 @@
-import { signOut } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { Button } from '../../components/Button';
+import { Link } from 'react-router-dom';
+import { LoadingSpinner } from '../../components/LoadingSpinner';
+import { SignOutButton } from '../../components/SignOutButton';
 import { useFirestoreUserContext } from '../../contexts/FirestoreUserContext';
-import { auth } from '../../firebase';
 import { user } from '../../types/user';
+import { useListContext } from './ListContext';
 
 export const ListHeader = () => {
   const { firestoreUser, loadingFirestoreUser } = useFirestoreUserContext();
-  const navigate = useNavigate();
+  const { list, loadingList } = useListContext();
 
   return (
-    <header className="flex gap-2 items-center justify-between px-4 py-2 text-lg shadow-md shadow-slate-300 border-b-2 border-slate-300 z-10 sticky bg-white top-0  w-full overflow-hidden">
-      {loadingFirestoreUser ? (
-        'Loading...'
+    <header className="flex gap-2 items-center justify-between px-4 py-2 text-lg shadow-md shadow-slate-300 border-b-2 border-slate-300 z-10 sticky bg-white top-0 w-full">
+      {loadingFirestoreUser || loadingList ? (
+        <LoadingSpinner />
       ) : (
         <>
-          <div className="ml-auto flex items-center gap-1">
-            {(firestoreUser as user).name}
-            <Button
-              onClick={async () => {
-                await signOut(auth);
-                navigate('/');
-              }}
-            >
-              Sign out
-            </Button>
+          <Link className="hover:underline" to="/">
+            Lists
+          </Link>
+          <h1 className="sm:absolute sm:left-1/2 sm:-translate-x-1/2 max-w-[20%] lg:max-w-[50%] overflow-hidden overflow-ellipsis font-bold max-w-1/2 self-center">
+            {list?.title}
+          </h1>
+          <div className="flex items-center gap-2">
+            <div className="max-w-[100px] lg:max-w-[200px] overflow-hidden whitespace-nowrap overflow-ellipsis">
+              {(firestoreUser as user).name}
+            </div>
+            <SignOutButton />
           </div>
         </>
       )}
