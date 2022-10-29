d520e9951a7b33e7d0c947bb786f2498550a70af
hdito <hditow@gmail.com>
2022-10-29 13:27:43 +0300

    Update loading indicator and sign out button in lists header

--- src/pages/lists/ListsHeader.tsx
+++ src/pages/lists/ListsHeader.tsx
@@ -1,30 +1,20 @@
-import { signOut } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { Button } from '../../components/Button';
+import { LoadingSpinner } from '../../components/LoadingSpinner';
+import { SignOutButton } from '../../components/SignOutButton';
 import { useFirestoreUserContext } from '../../contexts/FirestoreUserContext';
-import { auth } from '../../firebase';
 import { user } from '../../types/user';
 
 export const ListsHeader = () => {
   const { firestoreUser, loadingFirestoreUser } = useFirestoreUserContext();
-  const navigate = useNavigate();
 
   return (
     <header className="flex gap-2 items-center justify-between px-4 py-2 text-lg shadow-md shadow-slate-300 border-b-2 border-slate-300 z-10 sticky bg-white top-0  w-full overflow-hidden">
       {loadingFirestoreUser ? (
-        'Loading...'
+        <LoadingSpinner />
       ) : (
         <>
-          <div className="ml-auto flex items-center gap-1">
-            {(firestoreUser as user).name}
-            <Button
-              onClick={async () => {
-                await signOut(auth);
-                navigate('/');
-              }}
-            >
-              Sign out
-            </Button>
+          <div className="ml-auto flex items-center gap-2">
+            <div>{(firestoreUser as user).name}</div>
+            <SignOutButton />
           </div>
         </>
       )}
